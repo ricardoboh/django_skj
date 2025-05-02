271691c50800faca41521e6d4faa1e18e09381dc
ricardoboh <risa.bohunovsky@gmail.com>
2025-05-02 21:29:08 +0200

    delivery app init

--- delivery/models.py
+++ delivery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from person.models import DeliveryGuy
+from order.models import Order
 
-# Create your models here.
+class DeliveryInfo(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    delivery_guy = models.ForeignKey(DeliveryGuy, on_delete=models.CASCADE)
+    state_time = models.DateTimeField()
+    state = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.state} for Order #{self.order.id}"
